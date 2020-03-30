d737baa77caf0592afdd31a4723611e8e262ecbb
admirmisini <admir.misini@gmail.com>
2020-03-30 21:16:33 +0200

    add errors

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,31 @@
 package users
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/amisini/bookstore_users-api/domain/users"
+	"github.com/amisini/bookstore_users-api/services"
+	"github.com/amisini/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement me")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveError := services.CreateUser(user)
+	if saveError != nil {
+		c.JSON(saveError.Status, saveError)
+		return
+	}
+	fmt.Println(user)
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
