961d5f0b1cc34d2be8bed43d18a5ece5ccc80d52
admirmisini <admir.misini@gmail.com>
2020-03-30 19:52:33 +0200

    part1

--- app/url_mapping.go
+++ app/url_mapping.go
@@ -1,5 +1,13 @@
 package app
 
-func mapUrls() {
+import (
+	"github.com/amisini/bookstore_users-api/controllers/ping"
+	"github.com/amisini/bookstore_users-api/controllers/users"
+)
 
+func mapUrls() {
+	router.GET("/ping", ping.Ping)
+	router.GET("/users/:user_id", users.GetUser)
+	router.GET("/usersi/search", users.SearchUser)
+	router.POST("/users", users.CreateUser)
 }
